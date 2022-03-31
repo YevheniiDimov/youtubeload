701b4892f1e627bf14685368d5fc056c7be5997b
”YevheniiDimov” <dimovj12@gmail.com>
2022-03-31 16:35:51 +0200

    Implemented basic features

--- home/views.py
+++ home/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from pytube import YouTube
 
 def index(request):
-    return render(request, 'home/index.html')
+    context = {"message": ""}
+    if request.method == 'POST':
+        link = request.POST.get('link')
+        quality = request.POST.get('quality')
+        
+        yt = YouTube(link)
+        
+        file = yt.streams.filter(file_extension='mp4').get_by_resolution(quality)
+        
+        if file == None:
+            context['message'] = "Video doesn't support this quality"
+            return render(request, 'index.html', context)
+        
+        context = {"file": file, "message": "There is your video"}
+    
+    return render(request, 'index.html', context)
